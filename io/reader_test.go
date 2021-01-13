90846ec43f2dc4c27f0ced61859363a1a5664a18
Adrian Witas <adrianwit@gmail.com>
2021-01-13 09:32:19 -0800

    added test scafolding

--- io/reader_test.go
+++ io/reader_test.go
@@ -9,28 +9,37 @@ import (
 
 
 func TestReader_ReadAll(t *testing.T) {
-	var useUses = []struct{
+	var useCases = []struct{
 		description string
 		query string
 		newRow func() interface{}
 		params []interface{}
 		expect []interface{}
+		initSQL []string
 	} {
 
 	}
 
 
-	for _, useCase := range useUses {
+	outer: for _, useCase := range useCases {
 		ctx := context.Background()
 		var db *sql.DB
+
+		for _, SQL := range useCase.initSQL {
+			_, err := db.Exec(SQL)
+			if ! assert.Nil(t, err, useCase.description) {
+				continue outer
+			}
+		}
+
 		reader, err := NewReader(ctx, db, useCase.query, useCase.newRow)
-		assert.Nil(t, err, useUses)
+		assert.Nil(t, err, useCases)
 		var actual = make([]interface{}, 0)
 		err = reader.ReadAll(ctx, func(row interface{}) error {
 			actual = append(actual, row)
 			return nil
 		})
-		assert.Nil(t, err, useUses)
+		assert.Nil(t, err, useCases)
 
 	}
 
