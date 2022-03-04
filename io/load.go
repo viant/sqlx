e2b9c59b27a88502edc2973bf61c943bf680a9bf
KamLar <kamil.larysz.gra2@gmail.com>
2022-03-04 19:20:43 +0100

    fixed session exec method signature

--- io/load.go
+++ io/load.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"github.com/viant/sqlx/metadata/info"
+	"github.com/viant/sqlx/option"
 )
 
 //SessionResolver returns new Session configured with given Dialect
@@ -11,5 +12,5 @@ type SessionResolver = func(dialect *info.Dialect) Session
 
 //Session represents load session e.g. MySQL "LOAD DATA LOCAL INFILE"
 type Session interface {
-	Exec(context context.Context, data interface{}, db *sql.DB, tableName string) (sql.Result, error)
+	Exec(context context.Context, data interface{}, db *sql.DB, tableName string, options ...option.Option) (sql.Result, error)
 }
